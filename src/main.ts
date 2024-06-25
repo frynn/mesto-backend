b96624d38fad2bf83624903701247f06dccf604c
frynn <grodnenec04@mail.ru>
2024-06-25 08:26:21 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import * as cors from 'cors';
 
 declare module 'express' {
   interface Request {
@@ -13,6 +14,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:4200', // Замените на URL вашего фронтенда
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    }),
+  );
 }
 
 bootstrap();
