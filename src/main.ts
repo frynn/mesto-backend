743f178676a5ba0acb194717585416d5b829fcd0
frynn <grodnenec04@mail.ru>
2024-06-25 09:22:48 +0300

    added origin

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import * as cors from 'cors';
 
 declare module 'express' {
   interface Request {
@@ -10,16 +11,17 @@ declare module 'express' {
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
+  const app = await NestFactory.create(AppModule, { cors: { origin: true } });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+
+  app.use(
+    cors({
       origin: 'http://localhost:4200', // Замените на URL вашего фронтенда
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
-    },
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+    }),
+  );
 }
 
 bootstrap();
